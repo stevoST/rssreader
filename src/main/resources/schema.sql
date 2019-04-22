5f126f9dc2c21e1c39b75c7dd590522581fe464b
stevoST <stefan.starosta@gmail.com>
2019-04-22 22:32:09 +0200

    sql schema added

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS articles;
+
+CREATE TABLE articles (
+id BIGINT NOT NULL PRIMARY KEY,
+title VARCHAR(40),
+description VARCHAR(100),
+link VARCHAR(40),
+pubdate VARCHAR(40),
+);
