068bc981dbeb493e9b9fb9e881d137c9dbbaf914
stevoST <stefan.starosta@gmail.com>
2019-05-06 00:04:38 +0200

    lombok added. changed feed url

--- src/main/java/com/domain/Entity/Article.java
+++ src/main/java/com/domain/Entity/Article.java
@@ -1,9 +1,11 @@
 package com.domain.Entity;
 
+import lombok.Data;
+
 import javax.persistence.*;
 
 @Entity
-//@Table(uniqueConstraints = {@UniqueConstraint(columnNames = {"link"})})
+@Data
 public class Article {
 
     @Id
@@ -12,48 +14,7 @@ public class Article {
 
     private String title;
 
-//    @Column(name = "link")
     private String link;
     private String description;
     private String pubDate;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getLink() {
-        return link;
-    }
-
-    public void setLink(String link) {
-        this.link = link;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getPubDate() {
-        return pubDate;
-    }
-
-    public void setPubDate(String pubDate) {
-        this.pubDate = pubDate;
-    }
 }
