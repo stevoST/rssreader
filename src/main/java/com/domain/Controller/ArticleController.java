ed4f1a1e707fbe1b5dad41090b74fb46cf154005
stevoST <stefan.starosta@gmail.com>
2019-05-27 21:47:03 +0200

    fixed cors cross origin resource sharing

--- src/main/java/com/domain/Controller/ArticleController.java
+++ src/main/java/com/domain/Controller/ArticleController.java
@@ -3,11 +3,13 @@ package com.domain.Controller;
 import com.domain.Entity.Article;
 import com.domain.Service.ArticleService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RequestMapping("/")
 public class ArticleController {
 
