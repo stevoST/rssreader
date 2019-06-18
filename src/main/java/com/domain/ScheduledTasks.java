486356d3a8a8f1337b0f947197815e0cd3cb1cb7
stevoST <stefan.starosta@gmail.com>
2019-06-18 23:22:34 +0200

    tested feedlinks from db to list

--- src/main/java/com/domain/ScheduledTasks.java
+++ src/main/java/com/domain/ScheduledTasks.java
@@ -1,5 +1,6 @@
 package com.domain;
 
+import com.domain.Entity.Configuration;
 import com.domain.Service.ArticleService;
 import com.domain.Service.ConfigurationService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,8 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class ScheduledTasks {
@@ -14,6 +17,7 @@ public class ScheduledTasks {
     @Autowired
     private ArticleService articleService;
 
+    @Autowired
     private ConfigurationService configurationService;
 
 //    private static final Logger log = LoggerFactory.getLogger(ScheduledTasks.class);
@@ -25,8 +29,9 @@ public class ScheduledTasks {
     public void test() throws IOException {
 //        log.info("The time is now {}", dateFormat.format(new Date()));
 
-//        Iterable<Configuration> configurations = configurationService.readConfigurationFromDB();
-//        configurations.forEach(configuration -> configuration.getFeedLink());
+        Iterable<Configuration> configurations = configurationService.readConfigurationFromDB();
+        List<String> feedLinksList = new ArrayList<>();
+        configurations.forEach(configuration -> feedLinksList.add(configuration.getFeedLink()));
 
         String feedLink = "http://www.dsl.sk/export/rss_articles.php";
         articleService.getArticles(feedLink);
