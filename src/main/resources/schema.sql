b875ab194a189a1506165a8578f95d36a7be1293
stevoST <stefan.starosta@gmail.com>
2019-07-12 17:21:29 +0200

    added column feed_item

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -9,5 +9,6 @@ title varchar (256)
 create table if not exists configuration (
 id bigint not null AUTO_INCREMENT PRIMARY KEY,
 feed_name varchar (256),
-feed_link varchar (256) unique
+feed_link varchar (256) unique,
+feed_item varchar (256)
 );
