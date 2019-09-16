080399b7a93ad7f5a714f612fb5490b5b3801215
stevoST <stefan.starosta@gmail.com>
2019-09-16 23:01:12 +0200

    sorting by date added to config view articles

--- src/main/java/com/domain/Service/ArticleRepository.java
+++ src/main/java/com/domain/Service/ArticleRepository.java
@@ -9,6 +9,6 @@ import java.util.List;
 public interface ArticleRepository extends JpaRepository<Article,Integer> {
     Boolean existsByLink(String link);
 
-    @Query(value = "SELECT * FROM Article u WHERE u.feed_name = ?1", nativeQuery = true)
+    @Query(value = "SELECT * FROM Article u WHERE u.feed_name = ?1 ORDER BY u.pub_date_formatted DESC", nativeQuery = true)
     List<Article> findAllArticlesByFeedName(String feedName);
 }
